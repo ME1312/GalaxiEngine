82a02918fabbbb336367fa2f3c34fd14f08e2e30
ME1312 <admin@me1312.net>
2019-05-30 22:30:46 -0400

    Fix newline problems in Primitive Loggers

--- GalaxiAPI/src/net/ME1312/Galaxi/Library/Log/PrimitiveLogHandler.java
+++ GalaxiAPI/src/net/ME1312/Galaxi/Library/Log/PrimitiveLogHandler.java
@@ -25,7 +25,7 @@ public final class PrimitiveLogHandler extends Handler {
         if (open) {
             LogStream stream = log.info;
 
-            if (record.getLevel().intValue() == OFF.intValue()) {
+            if (record.getLevel().intValue() == OFF.intValue()) { // Translate Log Level
                 stream = null;
             } else if (record.getLevel().intValue() == FINE.intValue() || record.getLevel().intValue() == FINER.intValue() || record.getLevel().intValue() == FINEST.intValue()) {
                 stream = log.debug;
@@ -40,7 +40,7 @@ public final class PrimitiveLogHandler extends Handler {
             if (stream != null) {
                 String message = record.getMessage();
                 int i = 0;
-                if (record.getParameters() != null) for (Object obj : record.getParameters()) {
+                if (record.getParameters() != null) for (Object obj : record.getParameters()) {// Parse Parameters
                     String value;
                     if (obj instanceof Throwable) {
                         StringWriter sw = new StringWriter();
@@ -50,14 +50,16 @@ public final class PrimitiveLogHandler extends Handler {
                         value = Util.getDespiteException(() -> (obj == null)?"null":obj.toString(), "");
                     }
 
-                    if (message.contains("{" + i + "}")) {
+                    if (message.contains("{" + i + "}")) { // Write Parameters
                         message = message.replace("{" + i + "}", value);
                     } else {
                         message += "\n" + value;
                     }
                     i++;
                 }
-                stream.println(message.replace("\r", ""));
+
+                message = message.replace("\r", ""); // Remove carriage returns (has special meaning in the Galaxi)
+                for (String m : ((message.contains("\n"))?message.split("\n"):new String[]{ message })) stream.println(m); // Properly format new lines (if they exist)
             }
         }
     }
