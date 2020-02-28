aff7b34626e2504ba0e6a1917a65157c2721ea3d
ME1312 <admin@me1312.net>
2020-02-28 12:53:54 -0500

    Add PrimitiveContainer

---
+++ GalaxiAPI/global/src/net/ME1312/Galaxi/Library/PrimitiveContainer.java
@@ -0,0 +1,32 @@
+package net.ME1312.Galaxi.Library;
+
+/**
+ * Primitive Container Class
+ *
+ * @param <V> Item
+ */
+public class PrimitiveContainer<V> {
+    public V value;
+
+    /**
+     * Creates a Container
+     *
+     * @param item Object to Store
+     */
+    public PrimitiveContainer(V item) {
+        value = item;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object instanceof PrimitiveContainer) {
+            if (value == null || ((PrimitiveContainer) object).value == null) {
+                return value == ((PrimitiveContainer) object).value;
+            } else {
+                return value.equals(((PrimitiveContainer) object).value);
+            }
+        } else {
+            return super.equals(object);
+        }
+    }
+}
